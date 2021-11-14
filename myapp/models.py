7125be1d2055baf22a45d7c44458593edd31e964
Rittwick Bhabak <rittwick2019@gmail.com>
2021-11-15 00:38:02 +0530

    final commit

--- myapp/models.py
+++ myapp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Language(models.Model):
     title = models.CharField(max_length=50)
+    full_name = models.CharField(max_length=60, default='')
 
     def __str__(self):
         return self.title
