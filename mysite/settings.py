42edd28b06864c34e347c30dce4dee1005da585e
Rittwick <rittwick2019@gmail.com>
2021-11-14 20:13:37 +0000

    from cloud

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g+c1mor-dv3lsxdwxd&d!a+w4g5a3q#m^b3((#q4irw4*$=@%^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tocdecidablilityclosedness.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
